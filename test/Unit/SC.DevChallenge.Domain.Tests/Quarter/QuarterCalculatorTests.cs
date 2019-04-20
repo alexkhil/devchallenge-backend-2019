a059cf1bd773927b93345d799f974c4c3c27b8e1
Alex Khil <alex.newman0123@gmail.com>
2019-04-20 15:24:39 +0300

    Add tests for domain calc

---
+++ test/Unit/SC.DevChallenge.Domain.Tests/Quarter/QuarterCalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using SC.DevChallenge.Domain.Quarter;
+using Xunit;
+
+namespace SC.DevChallenge.Domain.Tests.Quarter
+{
+    public class QuarterCalculatorTests
+    {
+        [Theory]
+        [InlineData(1, 1, 0)]
+        [InlineData(3, 1, 0)]
+        [InlineData(1, 5, 1)]
+        [InlineData(3, 5, 3)]
+        public void GetQuarter_WhenCalled_ReturnCorrect(
+            int quarterNumber,
+            int timeslotElemCount,
+            int expected)
+        {
+            // Arrange
+            var sut = new QuarterCalculator();
+
+            // Act
+            var actual = sut.GetQuarter(quarterNumber, timeslotElemCount);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ test/Unit/SC.DevChallenge.Domain.Tests/Timeslot/TimeslotCalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using SC.DevChallenge.Domain.Timeslot;
+using Xunit;
+
+namespace SC.DevChallenge.Domain.Tests.Timeslot
+{
+    public class TimeslotCalculatorTests
+    {
+        [Theory]
+        [InlineData(0.75, 0.5, 0)]
+        public void GetLowerBound_WhenCalled_ReturnCorrect(
+            double averagePriceOfQuarter,
+            double interQuartileRange,
+            double expected)
+        {
+            // Arrange
+            var sut = new TimeslotCalculator();
+
+            // Act
+            var actual = sut.GetLowerBound(averagePriceOfQuarter, interQuartileRange);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(0.75, 0.5, 1.5)]
+        public void GetHigherBound_WhenCalled_ReturnCorrect(
+            double averagePriceOfQuarter,
+            double interQuartileRange,
+            double expected)
+        {
+            // Arrange
+            var sut = new TimeslotCalculator();
+
+            // Act
+            var actual = sut.GetHigherBound(averagePriceOfQuarter, interQuartileRange);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+    }
+}
